fdeafd86ac33f9bb3374d0c703a593662f6c6bed
David Wortham <djwortham@gmail.com>
2015-08-05 12:11:03 -0700

    Made URL parsing more robust (uses "url" and "queryparser" modules) and added support for signatures parsing

--- library.js
+++ library.js
@@ -1,21 +1,56 @@
 (function(module) {
     "use strict";
 
-    var AmazonAffiliate = {},
-        affiliateCode = 'daviwort05-20',
-        embed = '<a href="$1?' + affiliateCode + '" rel="nofollow">$2</a>';
+    // domain used to attempt to isolate errors to just the plugin
+    var domain = require('domain'),
+        d = domain.create();
 
+    d.on('error', function(err) {
+        console.error('nodebb-plugin-amazon-affiliate error: ' + err);
+    });
 
-    AmazonAffiliate.parse = function(data, callback) {
-        if (data.hasOwnProperty('postData')) {
-	    // data.postData.content = 'QQ: ' + data.postData.content;
-	    embed = '<a href="$1?tag=daviwor-5-20">$2</a>';
-            data.postData.content = data.postData.content.replace(/<a href="((:?https?:\/\/)?(:?[wW][wW][wW]\.)?[aA][mM][aA][zZ][oO][nN]\.[cC][oO][mM]\/[^"]*)".*>(.+)<\/a>/g, embed);
-            // " fix syntax coloring on some editors
-        }
+    d.run(function () {
+        var url = module.parent.require('url'), // https://nodejs.org/api/url.html
+            Entities = module.parent.require('html-entities').Html5Entities, // https://www.npmjs.com/package/html-entities
+            entities = new Entities(),
+            AmazonAffiliate = {},
+            affiliateCode = 'daviwort05-20';
 
-	callback(null, data);
-    };
+        module.exports = AmazonAffiliate;
 
-    module.exports = AmazonAffiliate;
+        // common replacement code
+        AmazonAffiliate.replaceAmazonLinks = function(fromText) {
+            var link_rewrite = function (match, p1, offset, search_string) {
+                    var parsed_matched_url = url.parse(entities.decode(p1), true, true),
+                        match1_len = p1.length,
+                        p1_match_offset = match.indexOf(p1);
+                    parsed_matched_url.query['tag']= affiliateCode;
+                    parsed_matched_url.search = undefined; // required to get "url" module to use the "query" dict instead of the "search" string
+
+                    // glue the new html entitized URL inside match
+                    return match.substring(0, p1_match_offset) + entities.encode(url.format(parsed_matched_url)) + match.substring(p1_match_offset + match1_len);
+                };
+
+            return fromText.replace(/<a href="((?:https?:\/\/)?(?:www\.)?amazon\.com\/[^"]*)".*>.+<\/a>/ig, link_rewrite);
+        };
+
+        // for posts
+        AmazonAffiliate.parsePost = function(data, callback) {
+            if (data.hasOwnProperty('postData')) {
+                data.postData.content = AmazonAffiliate.replaceAmazonLinks(data.postData.content);
+            }
+
+            callback(null, data);
+        };
+
+        // for user signatures
+        AmazonAffiliate.parseSignature = function(data, callback) {
+            if (data.hasOwnProperty('userData') && data.userData.hasOwnProperty('signature')) {
+                data.userData.signature = AmazonAffiliate.replaceAmazonLinks(data.userData.signature);
+            }
+
+            callback(null, data);
+        };
+    });
+
 }(module));
