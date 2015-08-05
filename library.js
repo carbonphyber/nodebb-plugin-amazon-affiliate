6427a3f0796811687999be19cb1edc28309a871b
David Wortham <djwortham@gmail.com>
2015-08-04 18:33:47 -0700

    Updating plugin API for recent versions of NodeBB

--- library.js
+++ library.js
@@ -2,12 +2,18 @@
     "use strict";
 
     var AmazonAffiliate = {},
-        embed = '<a href="$1?tag=daviwort05-20">$2</a>';
+        embed = '<a href="$1?tag=daviwort05-20" rel="nofollow">$2</a>';
 
 
-    AmazonAffiliate.parse = function(postContent, callback) {
-	postContent = postContent.replace(/<a href="((?:https?:\/\/)?(?:(?:www\.)?amazon\.com)\/.+)">(.+)<\/a>/g, embed);
-	callback(null, postContent);
+    AmazonAffiliate.parse = function(data, callback) {
+        if (data.hasOwnProperty('postData')) {
+	    // data.postData.content = 'QQ: ' + data.postData.content;
+	    embed = '<a href="$1?tag=daviwor-5-20">$2</a>';
+            data.postData.content = data.postData.content.replace(/<a href="((:?https?:\/\/)?(:?[wW][wW][wW]\.)?[aA][mM][aA][zZ][oO][nN]\.[cC][oO][mM]\/[^"]*)".*>(.+)<\/a>/g, embed);
+            // " fix syntax coloring on some editors
+        }
+
+	callback(null, data);
     };
 
     module.exports = AmazonAffiliate;
